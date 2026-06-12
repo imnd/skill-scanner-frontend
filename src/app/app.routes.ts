32cd2618b1e92dbbd5ce2e2f89324ae7dd2ac522
andreyserdyuk-sys <andrey.serdyuk@creativecode.digital>
2026-06-12 18:37:23 +0530

    courses page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Courses } from '@/app/pages/courses/courses';
 
 export const routes: Routes = [
   {
@@ -8,8 +7,24 @@ export const routes: Routes = [
       import('./pages/home/home.routes').then(m => m.HOME_ROUTES)
   },
   {
+    path: 'courses-categories',
+    loadComponent: () => import('./pages/courses-categories/courses-categories').then(m => m.CoursesCategories)
+  },
+  {
     path: 'courses',
     loadComponent: () => import('./pages/courses/courses').then(m => m.Courses)
+  },
+  {
+    path: 'schools',
+    loadComponent: () => import('./pages/schools/schools').then(m => m.Schools)
+  },
+  {
+    path: 'sales',
+    loadComponent: () => import('./pages/sales/sales').then(m => m.Sales)
+  },
+  {
+    path: 'posts',
+    loadComponent: () => import('./pages/posts/posts').then(m => m.Posts)
   },
   {
     path: '**',
--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { setBreadcrumbs } from '@/store/breadcrumbs/breadcrumbs.actions';
+import { CoursesList } from '@/components/courses/courses-list/courses-list';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [ CoursesList ],
   templateUrl: './courses.html',
-  styleUrl: './courses.css',
+  styleUrl: './courses.scss',
 })
 export class Courses implements OnInit {
   private readonly store = inject(Store);
